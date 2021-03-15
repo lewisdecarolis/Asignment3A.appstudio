c6aa9f50d1be87c8ad0288ad13758ff925338db9
lewisdecarolis <lsd81882@creighton.edu>
2021-03-14 21:44:50 -0500

    Initial commit

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,20 @@
+/*
+//Declares variables
+let cities = []
+let doneWithCities = false 
+let i = 0
+
+//Loop to determine how many cities the user enters
+while (doneWithCities == false) {
+    cities[i] = prompt ("Enter the next city:")
+    i++
+    doneWithCities = confirm ("Are you done entering cities?")    
+  }    
+
+//Declares new array
+let allCities = cities
+
+//Loop to log cities on seperate lines
+for(i = 0; i < allCities.length; i++)
+console.log(allCities[i])
+*/
